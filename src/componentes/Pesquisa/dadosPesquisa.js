98c1fc5d6ef70066bf04f935a8db9599803249df
Gustavo Cardoso Santana <gustavosantanaa02@gmail.com>
2023-09-11 22:44:04 -0300

    :contruction: componentes

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,12 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome:'Liderança em Design' , id:1, src:livro},
+    {nome:'Clean Code' , id:2, src:livro},
+    {nome:'Effective Java' , id:3, src:livro},
+    {nome:'Design Patterns' , id:4, src:livro},
+    {nome:'Python Crash Course' , id:5, src:livro},
+    {nome:'Introduction to the Theory of Computation' , id:6, src:livro},
+]
+
+ 
